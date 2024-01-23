3e7e4566d7283f9b5a1f05207fcf2ca8e515c819
Hayato Kawai <fohte.hk@gmail.com>
2024-01-24 02:28:20 +0900

    fix eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,3 @@
-const { mainConfig, typescriptConfig } = require('@fohte/eslint-config')
-
-module.exports = [...mainConfig, ...typescriptConfig]
+// workaround for eslint not supporting mjs
+// ref: https://zenn.dev/teppeis/scraps/c62621db4384d2
+module.exports = import('./eslint.config.mjs').then((n) => n.default)
